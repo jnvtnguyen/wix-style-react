0f2471ac51a41597651422841284da528ac18633
Netanel Abekasis <netanela@wix.com>
2021-02-24 16:58:13 +0200

    update examples

--- packages/wix-style-react/src/Themes/businessDashboard/docs/components/TableListItem.js
+++ packages/wix-style-react/src/Themes/businessDashboard/docs/components/TableListItem.js
@@ -2,18 +2,35 @@ export const playground = `
   <ThemeProvider theme={theme()}>
     <Card>
       <Card.Content>
-        <Layout cols={1}>
-          <TableListItem
-            verticalPadding="small"
-            showDivider
-            options={[
-              { value: 'Personal Finance' },
-              { value: '7 posts' },
-              { value: 'Last update on 27 April 2020' },
-            ]}
-            onClick={() => { }}
-          />
-        </Layout>
+        <TableListItem
+          verticalPadding="small"
+          showDivider
+          options={[
+            { value: 'First and onClick' },
+            { value: '7 posts' },
+            { value: 'Last update on 27 April 2020' },
+          ]}
+          onClick={() => { }}
+        />
+        <TableListItem
+          verticalPadding="small"
+          showDivider
+          options={[
+            { value: 'Second' },
+            { value: '7 posts' },
+            { value: 'Last update on 28 April 2020' },
+          ]}
+        />
+        <TableListItem
+          verticalPadding="small"
+          showDivider
+          options={[
+            { value: 'Third and onClick' },
+            { value: '7 posts' },
+            { value: 'Last update on 28 April 2020' },
+          ]}
+          onClick={() => { }}
+        />
       </Card.Content>
     </Card>
   </ThemeProvider>
