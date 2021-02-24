f6f4015efc6d6482c0f3f9d33c06bb1e36dad7e6
Karolis Daugerdas <karolisd@wix.com>
2021-02-24 17:03:12 +0200

    <Table/> - update empty state example according to guidelines

--- packages/wix-style-react/src/Table/docs/examples/17_TableEmptyStateExample.js
+++ packages/wix-style-react/src/Table/docs/examples/17_TableEmptyStateExample.js
@@ -20,20 +20,8 @@
   return (
     <Card>
       <Table>
-        <TableToolbar>
-          <TableToolbar.ItemGroup>
-            <TableToolbar.Item>
-              <TableToolbar.Title>Published</TableToolbar.Title>
-            </TableToolbar.Item>
-          </TableToolbar.ItemGroup>
-        </TableToolbar>
-        <Table.EmptyState
-          title="No Published Posts Yet"
-          subtitle="Once you publish posts, you'll see them here."
-          image={<Image height={120} width={120} borderRadius="60px" />}
-        >
-          <TextButton prefixIcon={<Icons.Add />}>Create New Post</TextButton>
-        </Table.EmptyState>
+        {renderTableToolbar()}
+        {renderEmptyState()}
       </Table>
     </Card>
   );
