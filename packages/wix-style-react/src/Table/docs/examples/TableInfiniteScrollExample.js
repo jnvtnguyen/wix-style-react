ec1734f80b3b9685dbb1eb25b4e5c210b158b308
Karolis Daugerdas <karolisd@wix.com>
2021-02-27 18:14:44 +0200

    <Table/> - POC infinite scroll example with fetch storybook util

--- packages/wix-style-react/src/Table/docs/examples/TableInfiniteScrollExample.js
+++ packages/wix-style-react/src/Table/docs/examples/TableInfiniteScrollExample.js
@@ -1,62 +1,52 @@
 /* eslint-disable */
 
-class TableInfiniteScrollExample extends React.Component {
-  state = {
-    count: 5,
-    hasMore: true,
-    baseData: [
-    { firstName: 'Meghan', lastName: 'Bishop' },
+() => {
+  const containerRef = React.useRef(null);
+  const [data, setData] = React.useState([
+    { firstName: 'Meghana', lastName: 'Bishop' },
     { firstName: 'Sara', lastName: 'Porter' },
     { firstName: 'Deborah', lastName: 'Rhodes' },
     { firstName: 'Walter', lastName: 'Jenning' },
-  ],
+  ]);
+  const [container, setContainer] = React.useState(null);
+  let loaded = 2;
+
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      loaded++;
+
+      Promise.resolve(StorybookUtils.fetch('/api/table', { load: loaded }))
+        .then(data => {
+          setData(data);
+        })
+        .catch(e => console.error(e));
+    }, 2000);
   };
 
-  componentDidMount() {
-    // Simulates a reference for rendered container element with maxHeight
-    this.setState({ container: this.containerRef });
-  }
+  React.useEffect(() => {
+    setContainer(containerRef);
+  }, []);
 
-  render() {
-    const { count, hasMore, container } = this.state;
-
-    return (
-      <div ref={ref => (this.containerRef = ref)} style={{ maxHeight: '258px', overflow: 'auto' }}>
-        <Table
-          data={this._generateData(count)}
-          columns={[
-            { title: 'First', render: row => row.firstName },
-            { title: 'Last', render: row => row.lastName },
-          ]}
-          infiniteScroll
-          // How many rows will be rendered on load
-          itemsPerPage={20}
-          // Indicates if there is more data to load
-          hasMore={hasMore}
-          // Function to be called on load
-          loadMore={this._fetchMoreData}
-          // Element with scroll events for listening
-          scrollElement={container}
-        >
-          <Table.Content />
-        </Table>
-      </div>
-    );
-  }
-
-  _fetchMoreData = async (baseData) => {
-    const loaded = 10;
-    const newData = await StorybookUtils.fetch('/api/table', { load: loaded + 5 });
-    this.setState({baseData: newData});
-  };
-
-  _generateData = count => {
-    let data = [];
-
-    for (let i = 0; i < count; i++) {
-      data = data.concat(this.state.baseData);
-    }
-
-    return data;
-  };
-}
+  const columns = [
+    { title: 'First', render: row => row.firstName },
+    { title: 'Last', render: row => row.lastName },
+  ];
+  return (
+    <div
+      ref={ref => (this.containerRef = ref)}
+      style={{ maxHeight: '258px', overflow: 'auto' }}
+    >
+      <Table
+        data={data}
+        columns={columns}
+        infiniteScroll
+        hasMore={true}
+        loadMore={fetchMoreData}
+        itemsPerPage={20}
+        scrollElement={container && container.current}
+      >
+        <Table.Content />
+      </Table>
+    </div>
+  );
+};
