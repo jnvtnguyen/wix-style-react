e1a5fd09866e6c52f8f7a7772c4673d28bf0cf7b
Egozi <egozieran@gmail.com>
2021-03-03 15:34:38 +0200

    Update packages/wix-style-react/stories/Playground/example.js

--- packages/wix-style-react/stories/Playground/example.js
+++ packages/wix-style-react/stories/Playground/example.js
@@ -1,2 +1,14 @@
 /* eslint-disable */
-<Text>Hello Привет</Text>
+<Card>
+  <Card.Header
+    title="Привет"
+    subtitle="Так выглядит субтитр"
+    suffix={<Button prefixIcon={<Icons.Add />}>Новый предмет</Button>}
+  />
+  <Card.Divider />
+  <Card.Content>
+    <FormField label="Метка поля" required>
+      <Input placeholder="Заполнитель" />
+    </FormField>
+  </Card.Content>
+</Card>
