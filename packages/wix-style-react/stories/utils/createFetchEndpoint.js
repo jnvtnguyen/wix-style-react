1cacca552128bc1ef90f99538613cb5eb2c9497f
Karolis Daugerdas <karolisd@wix.com>
2021-03-04 14:09:40 +0200

    <Table/> - add check whether URL is valid

--- packages/wix-style-react/stories/utils/createFetchEndpoint.js
+++ packages/wix-style-react/stories/utils/createFetchEndpoint.js
@@ -1,6 +1,21 @@
 export const createFetchEndpoint = (endpoint, initialData) => {
+  function isValidURL(str) {
+    const pattern = new RegExp(
+      '^(https?:\\/\\/)?' +
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+      'i',
+    ); //
+    return !!pattern.test(str);
+  }
+
   return api => {
-    const url = new URL(`https://www.random.com${api}`);
+    const url = isValidURL(api)
+      ? new URL(api)
+      : new URL(`https://random.com${api}`);
     if (url.pathname === endpoint) {
       return new Promise(resolve => {
         let results = initialData;
